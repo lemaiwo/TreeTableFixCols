17c4eb80459556717cd047c137e4bf590a1925f1
S0017014212 <wouter.lemaire@elia.be>
2019-01-29 12:35:18 +0000

    cleanup

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -9,25 +9,8 @@ sap.ui.define([
 		onInit: function () {
 			var oModel = new JSONModel("./model/Clothing.json");
 			this.getView().setModel(oModel);
-			Device.browser.chrome = true;
-			// var oTreeTable = this.byId("TreeTableBasic");
-			// oTreeTable._bIgnoreFixedColumnCount = false;
+			//Device.browser.chrome = true;
 		},
-		onAfterRendering: function () {
-			var oTreeTable = this.byId("TreeTableBasic");
-			// oTreeTable.addEventDelegate({
-			// 	onAfterRendering: function () {
-			// 		// debugger;
-			// 		var oCtrlScrDomRef = oTreeTable.getDomRef().querySelector(".sapUiTableCtrlScrFixed:not(.sapUiTableCHA) > .sapUiTableCtrlFixed");
-			// 		if (oCtrlScrDomRef) {
-			// 			// oCtrlScrDomRef.clientWidth = 700; //"700px";
-			// 			oCtrlScrDomRef.width = "700px";
-			// 		}
-			// 	}
-			// });
-
-		},
-
 		onCollapseAll: function () {
 			var oTreeTable = this.byId("TreeTableBasic");
 			oTreeTable.collapseAll();
